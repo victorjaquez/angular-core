bd6ab1395cfcc9ae9cc52e3c95c5f7a2aefbed7d
victorjaquez <victor@jaquez.me>
2019-04-07 16:20:33 -0700

    binded component data, event handling, ngIf & else

--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  projects = [
+    {
+      id: '1',
+      title: 'Project One',
+      details: 'This is a sample project',
+      percentComplete: 20,
+      approved: false
+    },
+    {
+      id: '2',
+      title: 'Project Two',
+      details: 'This is a sample project',
+      percentComplete: 40,
+      approved: false
+    },
+    {
+      id: '3',
+      title: 'Project Three',
+      details: 'This is a sample project',
+      percentComplete: 100,
+      approved: true
+    }
+  ];
+  selectedProject;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  selectProject(project) {
+    this.selectedProject = project;
+    console.log('SELECTED PROJECT', project);
   }
 
+  cancel() {
+    this.selectProject(null);
+  }
 }
