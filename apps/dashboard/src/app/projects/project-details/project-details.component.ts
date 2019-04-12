11cabecd92011d9c66bb4c8d95c7bcab32878939
victorjaquez <victor@jaquez.me>
2019-04-12 10:51:27 -0700

    fixed shared mutable state bug

--- apps/dashboard/src/app/projects/project-details/project-details.component.ts
+++ apps/dashboard/src/app/projects/project-details/project-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Project } from '@workshop/core-data';
 
 @Component({
   selector: 'app-project-details',
@@ -6,7 +7,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./project-details.component.scss']
 })
 export class ProjectDetailsComponent {
-  @Input() project;
+  currentProject: Project;
+  originalTitle;
   @Output() saved = new EventEmitter();
   @Output() cancelled = new EventEmitter();
+
+  // Fixes shared mutable state problem
+  // The "set" syntax binds an object property to a function to be called when there is an attempt to set that property
+  // Create local copy of state
+  // Copy the values f all enumerable properties to the target object in this case, an empty object.
+  // You are no longer dealing w/ application state, now you have presentational state
+  //
+  @Input()
+  set project(value) {
+    if (value) this.originalTitle = value.title;
+    this.currentProject = Object.assign({}, value);
+  }
 }
